8d8f35e189be43a447ab2876ed6d19e9db09b552
BeobSik <tjswl756@naver.com>
2022-03-09 15:24:52 +0900

    feat: try-with-resource

--- demoEffectiveJava/src/Item09/AppRunner.java
+++ demoEffectiveJava/src/Item09/AppRunner.java
@@ -5,20 +5,33 @@ public class AppRunner {
         /**
          * 고전적인 예외 처리 방법
          */
-        MyResource myResource=new MyResource();
-        try {
-            myResource.doSomething();
-            MyResource newMyResource = null;
-            try{
-                newMyResource=new MyResource();
-                newMyResource.doSomething();
-            }finally {
-                if(newMyResource!=null) {
-                    newMyResource.close();
-                }
-            }
-        }finally {
-            myResource.close();
+//        MyResource myResource=new MyResource();
+//        try {
+//            myResource.doSomething();
+//            MyResource newMyResource = null;
+//            try{
+//                newMyResource=new MyResource();
+//                newMyResource.doSomething();
+//            }finally {
+//                if(newMyResource!=null) {
+//                    newMyResource.close();
+//                }
+//            }
+//        }finally {
+//            myResource.close();
+//        }
+
+        /**
+         * try-with-resource
+         */
+//        try(MyResource myResource=new MyResource()) {
+//            myResource.doSomething();
+//        }
+
+        try(MyResource myResource1=new MyResource();
+            MyResource myResource2=new MyResource()) {
+            myResource1.doSomething();
+            myResource2.doSomething();
         }
     }
 }
