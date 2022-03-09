1ff93bc174d49ccf8e2cfad32dafbc561e98907b
BeobSik <tjswl756@naver.com>
2022-03-09 15:15:30 +0900

    feat: 고전적인 방식은 코드가 장황해진다.

--- demoEffectiveJava/src/Item09/AppRunner.java
+++ demoEffectiveJava/src/Item09/AppRunner.java
@@ -8,6 +8,15 @@ public class AppRunner {
         MyResource myResource=new MyResource();
         try {
             myResource.doSomething();
+            MyResource newMyResource = null;
+            try{
+                newMyResource=new MyResource();
+                newMyResource.doSomething();
+            }finally {
+                if(newMyResource!=null) {
+                    newMyResource.close();
+                }
+            }
         }finally {
             myResource.close();
         }
