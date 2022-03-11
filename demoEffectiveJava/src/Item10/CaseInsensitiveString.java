8ac70bc0acb3cedf85f7440e060307d46709e39a
BeobSik <tjswl756@naver.com>
2022-03-11 22:00:05 +0900

    feat: 대칭성 위반 코드

---
+++ demoEffectiveJava/src/Item10/CaseInsensitiveString.java
@@ -0,0 +1,23 @@
+package Item10;
+
+import java.util.Objects;
+
+public final class CaseInsensitiveString {
+    private final String s;
+
+    public CaseInsensitiveString(String s){
+        this.s= Objects.requireNonNull(s);
+    }
+
+    // 대칭성 위반 코드
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof CaseInsensitiveString){
+            return  s.equalsIgnoreCase(((CaseInsensitiveString)o).s);
+        }
+        if(o instanceof String){
+            return s.equalsIgnoreCase((String) o);
+        }
+        return false;
+    }
+}
---
+++ demoEffectiveJava/src/Item10/Main.java
@@ -0,0 +1,11 @@
+package Item10;
+
+public class Main {
+    public static void main(String[] args) {
+        CaseInsensitiveString cis=new CaseInsensitiveString("Polish");
+        String s="polish";
+
+        System.out.println(cis.equals(s));
+        System.out.println(s.equals(cis));
+    }
+}
